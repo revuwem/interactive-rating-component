4295eff3147d4062f05e17edc798d27de05171b8
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 02:26:54 +0400

    feat(Paragraph): add margin bottom style

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,7 +3,7 @@ interface IParagraphProps {
 }
 
 const Paragraph: React.FC<IParagraphProps> = ({ children }) => {
-  return <p className="font-sans text-base text-grey-200">{children}</p>;
+  return <p className="font-sans text-base text-grey-200 mb-8">{children}</p>;
 };
 
 export default Paragraph;
