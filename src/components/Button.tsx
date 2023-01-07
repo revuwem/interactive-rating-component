191b3f5d445ee943fb98ad583e757a35a23ccc02
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 01:30:28 +0400

    feat(Button): add style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,21 @@ interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button: React.FC<IButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+  return (
+    <button
+      className={`
+          font-sans text-base font-bold uppercase tracking-widest 
+          w-full p-4
+          text-white hover:text-primary
+          bg-primary hover:bg-white
+          rounded-full
+          transition
+        `}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
