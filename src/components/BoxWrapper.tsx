25f738be24e7f0a56328746bf5acef3b2114ae0d
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 02:03:15 +0400

    feat(BoxWrapper): add styles

--- src/components/BoxWrapper.tsx
+++ src/components/BoxWrapper.tsx
@@ -3,7 +3,23 @@ interface IBoxWrapper {
 }
 
 const BoxWrapper: React.FC<IBoxWrapper> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div
+      className={`
+        flex
+        flex-col
+        items-center
+        justify-center
+        max-w-sm
+        p-6
+        pr-8
+        rounded-2xl
+        bg-blue-200
+      `}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default BoxWrapper;
