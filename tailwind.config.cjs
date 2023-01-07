207722ec8cbc306c697b304ac43fa8e9d983eda2
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 00:40:54 +0400

    feat(tailwind): customize theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Overpass", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        base: "15px",
+      },
+      colors: {
+        primary: "hsl(25, 97%, 53%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: {
+          100: "hsl(216, 12%, 54%)",
+          200: "hsl(217, 12%, 63%) ",
+        },
+        blue: {
+          100: "hsl(216, 12%, 8%)",
+          200: "hsl(213, 19%, 18%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
