f62bd44726c9c33010b2cc18cb6102303e8d8a0a
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 23:39:46 +0400

    feat(Paragraph): remove mb if last child

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,7 +3,11 @@ interface IParagraphProps {
 }
 
 const Paragraph: React.FC<IParagraphProps> = ({ children }) => {
-  return <p className="font-sans text-base text-grey-200 mb-8">{children}</p>;
+  return (
+    <p className="font-sans text-base text-grey-200 mb-8 last:mb-0">
+      {children}
+    </p>
+  );
 };
 
 export default Paragraph;
