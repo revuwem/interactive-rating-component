32edca6401030db50903d6218e7dec296543b6b9
Karina Grishaeva <revuwem@gmail.com>
2023-01-08 23:39:12 +0400

    feat(Badge): add style

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,7 +3,11 @@ interface IBadgeProps {
 }
 
 const Badge: React.FC<IBadgeProps> = ({ children }) => {
-  return <p>{children}</p>;
+  return (
+    <p className="font-sans text-primary bg-blue-400 px-4 py-1 rounded-full mb-8">
+      {children}
+    </p>
+  );
 };
 
 export default Badge;
